2a68500573a5b338893f75300058efa014d4d488
Tom X Nguyen <tom81094@gmail.com>
2023-08-11 00:17:32 +0700

    chore: enable plausible analytics

---
+++ publish.js
@@ -0,0 +1,5 @@
+let plausibleSnippet = document.createElement('script');
+plausibleSnippet.defer = true;
+plausibleSnippet.setAttribute('data-domain', 'brain.d.foundation');
+plausibleSnippet.src = 'https://plausible.io/js/plausible.js';
+document.head.appendChild(plausibleSnippet);
